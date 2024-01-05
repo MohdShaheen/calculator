6934f924468bfbf93704bb8cb4d08187ffd26c47
MohdShaheen <mohammedshaheen916@gmail.com>
2024-01-05 16:18:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Calcul from './Component/Calcul.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calcul/>
+    </div>
+  );
+}
+
+export default App;
